ec8661e4f5c8681201560b15c8b7f14ad4b56652
Cate Uch <cat.uch@yandex.ru>
2021-11-18 02:55:45 +0300

    Mocks, API requests testing

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -1,13 +1,15 @@
 import { render, screen } from "@testing-library/react";
-import Async from './Async';
+import Async from "./Async";
 
+describe("Async component", () => {
+  test("renders post if request succeeds", async () => {
+    window.fetch = jest.fn()
+    window.fetch.mockResolvedValueOnce({  //allows to set a value this fetch
+        json: async () => [{id: 'p1', title: 'Firest post'}],
+    })
+    render(<Async />);
 
-describe ('Async component', () => {
-    test('renders post if request succeeds', async () => {
-        render(<Async />)
-
-        const listItemElements = await screen.findAllByRole('listitem'); // f'ind' returns a Promise
-        expect(listItemElements).not.toHaveLength(0)
-
-    });
-})
+    const listItemElements = await screen.findAllByRole("listitem"); // f'ind' returns a Promise
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
